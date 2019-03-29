3342b6111d1721f2fa2e939cbccc3e312fed11fb
John Dang <john.dang@trainadesign.com>
2019-03-29 10:42:04 -0700

    003 return negative

---
+++ 8 kyu/003-return-negative.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+
+function makeNegative(num) {
+    if (num > 0) {
+    return num * -1;
+    }
+    else {
+    return num;
+    }
+    };
