85eda58d09b4004008cf0ce336bdad6f1d19e0c1
John Dang <john.dang@trainadesign.com>
2019-04-03 09:34:50 -0700

    010 opposite number

---
+++ 8 kyu/010-opposite-number.js
@@ -0,0 +1,15 @@
+/* ==============================
+Opposite number
+https://www.codewars.com/kata/opposite-number/train/javascript
+
+Very simple, given a number, find its opposite.
+
+Examples:
+1: -1
+14: -14
+-34: 34
+============================== */
+
+function opposite(number) {
+    return -number;
+}
