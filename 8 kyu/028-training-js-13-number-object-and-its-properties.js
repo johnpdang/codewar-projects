65718bf5a01f1b2301da06cf52c8c7392d7724b0
John Dang <john.dang@trainadesign.com>
2019-04-30 09:18:18 -0700

    037 training js 22

--- 8 kyu/028-training-js-13-number-object-and-its-properties.js
+++ 8 kyu/028-training-js-13-number-object-and-its-properties.js
@@ -52,6 +52,13 @@ whatNumberIsIt(100) should return "Input number is 100"
 What you need to think about is how to judge it correctly and effectively and don't forget isNaN().
 ============================== */
 
-function whatNumberIsIt(n) {
-  //coding here
+function whatNumberIsIt(n){
+  switch (true) {
+    case (n==Number.MAX_VALUE): return "Input number is Number.MAX_VALUE";
+    case (n==Number.POSITIVE_INFINITY): return "Input number is Number.POSITIVE_INFINITY";
+    case (n==Number.MIN_VALUE): return "Input number is Number.MIN_VALUE";
+    case (n==Number.NEGATIVE_INFINITY): return "Input number is Number.NEGATIVE_INFINITY";
+    case (isNaN(n)): return "Input number is Number.NaN";
+    default: return "Input number is " + n;
+  }
 }
---
+++ 8 kyu/037-training-js-22.js
@@ -0,0 +1,9 @@
+/* ==============================
+Training JS #22: Unlock new skills--Arrow function,spread operator and deconstruction
+https://www.codewars.com/kata/training-js-number-22-unlock-new-skills-arrow-function-spread-operator-and-deconstruction/train/javascript
+============================== */
+
+function shuffleIt(arr,e){
+  //coding here...
+  
+}
