61cb5e139aab2b7f4501f94ab87cb8e431420074
John Dang <john.dang@trainadesign.com>
2019-05-01 09:40:53 -0700

    038 training js 23

---
+++ 8 kyu/038-training-js-23.js
@@ -0,0 +1,9 @@
+/* ==============================
+Training JS #23: methods of arrayObject---push(), pop(), shift() and unshift()
+https://www.codewars.com/kata/training-js-number-23-methods-of-arrayobject-push-pop-shift-and-unshift/train/javascript
+============================== */
+
+function infiniteLoop(arr,d,n){
+  //coding here...
+  
+}
