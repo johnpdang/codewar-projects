980be27884eca262a31586bdf8c94962a686299c
John Dang <john.dang@trainadesign.com>
2019-05-02 09:53:13 -0700

    039: training js 24

---
+++ 7 kyu/039-training-js-24.js
@@ -0,0 +1,9 @@
+/* ==============================
+Training JS #24: methods of arrayObject---splice() and slice()
+https://www.codewars.com/kata/572cb264362806af46000793
+============================== */
+
+function threeInOne(arr){
+  //coding here...
+  
+}
