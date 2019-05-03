27a692213bd888ce7e20c3475469950fac378dc1
John Dang <john.dang@trainadesign.com>
2019-05-03 09:31:33 -0700

    040: training js 25

---
+++ 7 kyu/040-training-js-25.js
@@ -0,0 +1,9 @@
+/* ==============================
+Training JS #25: methods of arrayObject---reverse() and sort()
+https://www.codewars.com/kata/572df796914b5ba27c000c90
+============================== */
+
+function sortIt(arr){
+  //coding here...
+  
+}
