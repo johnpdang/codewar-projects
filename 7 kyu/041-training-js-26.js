fbd3b5ea40a3b7813066b146f3e2dd3f357061b3
John <johnpdang@gmail.com>
2019-05-04 20:52:09 -0700

    041 training js 26

---
+++ 7 kyu/041-training-js-26.js
@@ -0,0 +1,8 @@
+/* ==============================
+Training JS #26: methods of arrayObject---map()
+https://www.codewars.com/kata/training-js-number-26-methods-of-arrayobject-map/train/javascript
+============================== */
+
+function isolateIt(arr) {
+  //coding here...
+}
