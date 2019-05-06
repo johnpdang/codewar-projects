37f4642967fde08711b53a2492c2feac48846baa
John Dang <john.dang@trainadesign.com>
2019-05-06 09:37:51 -0700

    043 training js 28

---
+++ 7 kyu/043-training-js-28.js
@@ -0,0 +1,9 @@
+/* ==============================
+Training JS #28: methods of arrayObject---every() and some()
+https://www.codewars.com/kata/training-js-number-28-methods-of-arrayobject-every-and-some/train/javascript
+============================== */
+
+function mirrorImage(arr){
+  //coding here...
+  
+}
