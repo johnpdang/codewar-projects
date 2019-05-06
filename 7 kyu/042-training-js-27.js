833df01f772074f80defa0ace31737403c3cf594
John <johnpdang@gmail.com>
2019-05-05 19:25:35 -0700

    042 training js 27

---
+++ 7 kyu/042-training-js-27.js
@@ -0,0 +1,8 @@
+/* ==============================
+Training JS #27: methods of arrayObject---filter()
+https://www.codewars.com/kata/training-js-number-27-methods-of-arrayobject-filter/train/javascript
+============================== */
+
+function countGrade(scores) {
+  //coding here...
+}
