dcbc2ce486a686f27065681b6567ffce93f6a5ab
John Dang <john.dang@trainadesign.com>
2019-05-07 15:00:14 -0700

    044 training js 29

---
+++ 7 kyu/044-training-js-29.js
@@ -0,0 +1,9 @@
+/* ==============================
+Training JS #29: methods of arrayObject---concat() and join()
+https://www.codewars.com/kata/training-js-number-29-methods-of-arrayobject-concat-and-join/train/javascript
+============================== */
+
+function bigToSmall(arr){
+  //coding here...
+  
+}
