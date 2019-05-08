263a7251dbb7ab254bb5afeb3ba9acc35c914739
John Dang <john.dang@trainadesign.com>
2019-05-08 09:10:42 -0700

    045 training js 30

---
+++ 7 kyu/045-training-js-30.js
@@ -0,0 +1,9 @@
+/* ==============================
+Training JS #30: methods of arrayObject---reduce() and reduceRight()
+https://www.codewars.com/kata/training-js-number-30-methods-of-arrayobject-reduce-and-reduceright/train/javascript
+============================== */
+
+function tailAndHead(arr){
+  //coding here...
+  
+}
