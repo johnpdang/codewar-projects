a265625c62ccb358f1e47f0543b6ec2483730ca9
John Dang <john.dang@trainadesign.com>
2019-05-09 11:00:40 -0700

    046 training js 31

---
+++ 8 kyu/046-training-js-31.js
@@ -0,0 +1,9 @@
+/* ==============================
+Training JS #31: methods of arrayObject---isArray() indexOf() and toString()
+https://www.codewars.com/kata/training-js-number-31-methods-of-arrayobject-isarray-indexof-and-tostring/train/javascript
+============================== */
+
+function blackAndWhite(arr){
+  //coding here...
+
+}
