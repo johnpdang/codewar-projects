b525059c35fb3d68cd190cdf57d411ff9f101fd8
John <johnpdang@gmail.com>
2019-05-10 08:00:01 -0700

    047 training js 32

---
+++ 8 kyu/047-training-js-32.js
@@ -0,0 +1,8 @@
+/* ==============================
+Training JS #32: methods of Math---round() ceil() and floor()
+https://www.codewars.com/kata/training-js-number-32-methods-of-math-round-ceil-and-floor/train/javascript
+============================== */
+
+function roundIt(n) {
+  //coding here...
+}
