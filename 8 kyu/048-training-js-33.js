f7c8239d2cac99e7ed1b15a6865c86db1b7d0f5b
John <johnpdang@gmail.com>
2019-05-11 07:01:33 -0700

    048 training js 33

---
+++ 8 kyu/048-training-js-33.js
@@ -0,0 +1,8 @@
+/* ==============================
+Training JS #33: methods of Math---max() min() and abs()
+https://www.codewars.com/kata/training-js-number-33-methods-of-math-max-min-and-abs/train/javascript
+============================== */
+
+function maxMin(arr1, arr2) {
+  //coding here...
+}
