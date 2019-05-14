fd58ada757c0ef840f3609898fde7a9cf1474175
John Dang <john.dang@trainadesign.com>
2019-05-14 11:28:26 -0700

    050 training js 35

---
+++ 7 kyu/050-training-js-35.js
@@ -0,0 +1,9 @@
+/* ==============================
+Training JS #35: methods of Math---log() and its family
+https://www.codewars.com/kata/training-js-number-35-methods-of-math-log-and-its-family/train/javascript
+============================== */
+
+function thinkingAndTesting(number,base){
+  //coding here...
+
+}
