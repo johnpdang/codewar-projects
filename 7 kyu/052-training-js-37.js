208e4f9431082e2931731e5ce4360b7094d18dc0
John Dang <john.dang@trainadesign.com>
2019-05-16 10:20:50 -0700

    052 training js 37

---
+++ 7 kyu/052-training-js-37.js
@@ -0,0 +1,9 @@
+/* ==============================
+Training JS #37: Unlock new weapon---RegExp Object
+http://www.codewars.com/kata/5735e39313c205fe39001173
+============================== */
+
+function countAnimals(animals,count){
+  //coding here...
+
+}
--- 8 kyu/046-training-js-31.js
+++ 8 kyu/046-training-js-31.js
@@ -4,6 +4,7 @@ https://www.codewars.com/kata/training-js-number-31-methods-of-arrayobject-isarr
 ============================== */
 
 function blackAndWhite(arr){
-  //coding here...
+  return        !Array.isArray(arr)?"It's a fake array":
+  arr.includes(5)&&arr.includes(13)?"It's a black array":"It's a white array"
 
 }
