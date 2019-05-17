12a35cbe104e47bde555c76f368d6df92bdb12be
John Dang <john.dang@trainadesign.com>
2019-05-17 09:09:17 -0700

    -m 053 training js 38

---
+++ 7 kyu/053-training-js-38.js
@@ -0,0 +1,8 @@
+/* ==============================
+Training JS #38: Regular Expression--"^","$", "." and test()
+https://www.codewars.com/kata/573975d3ac3eec695b0013e0
+============================== */
+
+function regEx(e) {
+  //coding here...
+}
