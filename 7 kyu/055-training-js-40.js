c0d1b62e215d33d67cc3ccab228e6147e9215d0e
John <johnpdang@gmail.com>
2019-05-19 19:11:29 -0700

    055 training js 40

---
+++ 7 kyu/055-training-js-40.js
@@ -0,0 +1,8 @@
+/* ==============================
+Training JS #40: Regular Expression--"|", "[]" and "()"
+https://www.codewars.com/kata/573d11c48b97c0ad970002d4
+============================== */
+
+function example(e) {
+  //coding here...
+}
