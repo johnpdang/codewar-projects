ce7aa2d1d1fcd91ea4e0fe42c5d6548124a83875
John Dang <john.dang@trainadesign.com>
2019-05-21 09:23:49 -0700

    057 training js 42

---
+++ 7 kyu/057-training-js-42.js
@@ -0,0 +1,8 @@
+/* ==============================
+Training JS #42: Regular Expression--( ?: ), ( ?= ) and ( ?! )
+https://www.codewars.com/kata/573fb9223f9793e485000453
+============================== */
+
+function example(e) {
+  //coding here...
+}
