5481fb17106f13b42f9e2fba24ec7602e054ba22
John Dang <john.dang@trainadesign.com>
2020-01-08 09:29:03 -0800

    058 will you make it

---
+++ 8 kyu/058-will-you-make-it.js
@@ -0,0 +1,12 @@
+/* ==============================
+Will you make it?
+https://www.codewars.com/kata/5861d28f124b35723e00005e/solutions/javascript
+============================== */
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  if (mpg*fuelLeft >= distanceToPump) {
+    return true;
+  } else {
+    return false;
+  }
+};
